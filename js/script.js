5e1bb3c18d42ad2aba1499ee4af58f188bbe5380
matthewjenifer <matthew36.j@gmail.com>
2023-04-27 20:50:17 -0700

    new nav

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function toggleNav() {
+    var navBar = document.getElementById("navBar");
+    if (navBar.style.display === "none") {
+      navBar.style.display = "block";
+    } else {
+      navBar.style.display = "none";
+    }
+  }
+  
